f105995765242033cf483b98e456f1f23d374f0f
Santiago Torres <santorres724@gmail.com>
2024-05-08 01:16:18 -0300

    Configuracion del proyecto

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaInventarioV6.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ SistemaInventarioV6.Modelos/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventarioV6.Modelos.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

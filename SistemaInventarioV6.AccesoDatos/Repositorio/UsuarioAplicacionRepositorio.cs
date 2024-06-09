c89e3d1ed21cbc98299c39ed47eeda66d31377b5
Santiago Torres <santorres724@gmail.com>
2024-06-09 01:52:03 -0300

    Modelo Usuario

---
+++ SistemaInventarioV6.AccesoDatos/Repositorio/UsuarioAplicacionRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using SistemaInventarioV6.AccesoDatos.Data;
+using SistemaInventarioV6.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioV6.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioV6.AccesoDatos.Repositorio
+{
+    public class UsuarioAplicacionRepositorio : Repositorio<UsuarioAplicacion>, IUsuarioAplicacionRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UsuarioAplicacionRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        
+    }
+}
